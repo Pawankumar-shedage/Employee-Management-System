0a7f5bd94ed7e5f5f997bce2cc1f54f3ffffb845
Pawankumar Shedage <pawankumarshedage@gmail.com>
2023-09-28 16:45:10 +0530

    Employee salary field added

--- src/main/java/com/Day2/entity/Employee.java
+++ src/main/java/com/Day2/entity/Employee.java
@@ -18,20 +18,28 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int emp_id;
-
-	
 	private String emp_name;
-	
-	
 	private String dept_name;
+	private double emp_salary;
 	
 	
 
-	public Employee(int emp_id, String emp_name, String dept_name) {
+	public Employee(int emp_id, String emp_name, String dept_name, double emp_salary) {
 		super();
 		this.emp_id = emp_id;
 		this.emp_name = emp_name;
 		this.dept_name = dept_name;
+		this.emp_salary = emp_salary;
+	}
+
+
+	public double getEmp_salary() {
+		return emp_salary;
+	}
+
+
+	public void setEmp_salary(double emp_salary) {
+		this.emp_salary = emp_salary;
 	}
 
 
